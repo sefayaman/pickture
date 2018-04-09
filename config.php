fc6546a130d611ab37eb9b51f4f03acf4d95eec0
RustyFox <fiona.sequeira891@gmail.com>
2018-04-09 18:10:38 -0400

    Adding composer,config and navigation header

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$aws_access_key_id = "YOUR_AWS_KEY_ID";
+$aws_secret_access_key = "YOUR_AWS_ACCESS_KEY";
+?>
---
+++ includes/navbar.php
@@ -0,0 +1,16 @@
+<a href="basehome.php" color=white><h4> pickture  <img src="cart.png" name="logout" width="100" height="70"></h4></a>
+<button><a href="home.php" class="button">  LIVEFEED </a></button>';
+<!-- Keyword search tab -->
+	<form class="search" action="../app/search_results.php" align="left" method="post">       
+      <div class="searchbox">
+        <input type="text" class="form-control" name="Name" placeholder="Search Network" />
+        <input type="image" alt="Submit" align="center" src="search_human.png" name="searchbutton"  width="50" height="50"></div><br>
+        <br>
+      </div>
+    </form> 
+    <!-- Logout from the system functionality -->
+    <a href="logout.php">
+    <img align="right" style="float:right; margin-top: -140px; display: inline-block; padding: 10px 10px;"src="Power_button.png" name="logout" width="80" height="80">
+    </a>
+
+	
